79902e803a693d7e1d27d54051e981645a8c19e9
Samudra <samudra@rvtechnologies.com>
2019-05-14 15:36:55 +0530

    Test integration

--- app.js
+++ app.js
@@ -17,3 +17,5 @@ const serverOptions = {
 let app = Server(serverOptions)
 
 app.listen(3000, _ => console.log("Server is up and running at 3000"))
+
+module.exports = app
--- urlConfig/index.js
+++ urlConfig/index.js
@@ -2,6 +2,7 @@ const { render, Routify } = require("lane-js")
 
 let route = new Routify();
 
-route.get("/", (req, res) => render(res, "index.ejs", { title: "LaneJs" }));
+// route.get("/", (req, res) => render(res, "index.ejs", { title: "LaneJs" }));
+route.get("/", (req, res) => res.end("Hello World"));
 
 module.exports = route.expose();
